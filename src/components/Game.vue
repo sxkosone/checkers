d94dead5db3374e627748d20410ba7a9b1847173
Michele Cynowicz <michele.cynowicz@voxmedia.com>
2018-06-01 15:17:02 -0400

    updated inputs for double-data-binding and data return to pass both player objects. Also updated eslint config to accept 2-spaces for indents

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -8,18 +8,14 @@
 <script>
 // import chipsState from './../../data/chips_state.json'
 // console.log('chipsState', chipsState)
-import playerOne from './../../data/player1.json'
-import playerTwo from './../../data/player2.json'
-
 import RegisterPlayers from './RegisterPlayers.vue'
 console.log(RegisterPlayers)
-console.log('playerOne', playerOne)
-console.log('playerTwo', playerTwo)
+
 export default {
   name: 'Checkers',
-  components: { 
-       RegisterPlayers 
-      }, 
+  components: {
+    RegisterPlayers
+  },
   data () {
     return {
       msg: 'Welcome to Vue Checkers'
--- src/components/RegisterPlayers.vue
+++ src/components/RegisterPlayers.vue
@@ -2,22 +2,26 @@
   <div class="register-players">
     <form>
       <label>What's your name, Player One?</label>
-      <input
-        v-bind:value="playerOne.name"
-        @change="playerOne.update" />
+      <input v-model="playerOne.name" />
+      <br /><br /><hr /><br />
+      <label>What's your name, Player Two?</label>
+      <input v-model="playerTwo.name" />
     </form>
   </div>
 </template>
 
 <script>
+import playerOne from './../../data/player1.json'
+import playerTwo from './../../data/player2.json'
+
+console.log('playerOne', playerOne)
+console.log('playerTwo', playerTwo)
 export default {
   name: 'RegisterPlayers',
   data () {
     return {
-      playerOne: {
-        name: 'Michele',
-        update: (e) => { console.log(e.target.value) }
-      }
+      playerOne: playerOne,
+      playerTwo: playerTwo
     }
   }
 }
