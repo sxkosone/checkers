29eb8c52b4c4b99f393a80b36d378a1593a4dee3
Michele Cynowicz <michele.cynowicz@voxmedia.com>
2018-06-02 14:01:32 -0400

    new gameSquares.json file and import of this into Board.vue

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,18 +1,21 @@
 <template>
   <div v-if="gameStats.gameStart">
     <h2>{{ msg }}</h2>
+    
   </div>
 </template>
 
 <script>
 import gameStats from './../../data/gameStats.json'
+import gameSquares from './../../data/gameSquares.json'
 
 export default {
   name: 'Board',
   data () {
     return {
       msg: 'This is the Board!',
-      gameStats: gameStats
+      gameStats: gameStats,
+      gameSquares: gameSquares
     }
   }
 }
